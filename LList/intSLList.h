432062fbf2da5baaefd1703664421fcd9c0a87e7
jasonmwalker93 <jasonmwalker93@gmail.com>
2017-04-20 17:17:45 -0400

    Add files via upload

---
+++ LList/intSLList.h
@@ -0,0 +1,50 @@
+#ifndef INTSLLIST
+#define INTSLLIST
+using namespace std;
+
+class IntSLLNode {
+public:
+	IntSLLNode() 
+	{
+		next = 0;
+	}
+	IntSLLNode(int el, IntSLLNode *ptr = 0) 
+	{
+		info = el; next = ptr;
+	}
+	int info;
+	IntSLLNode *next;
+};
+class IntSLList 
+{
+public:
+	IntSLList() 
+	{
+		head = tail = 0;
+	}
+	~IntSLList();
+	int isEmpty() 
+	{
+		return head == 0;
+	}
+	void addToHead(int);
+	void addToTail(int);
+	int deleteFromHead();	// delete the head and return its info;
+	int deleteFromTail();	// delete the tail and return its info;
+	void deleteNode(int);
+	bool isInList(int) const;
+	void displayList();
+	int listCount();
+	void multiply(int);
+	int frequency(int);
+	void deleteAll();
+	void reverse();
+	void appendList(IntSLList&);
+	void removeNode(int);
+	void removeNodes(int, int);
+
+private:
+	IntSLLNode *head, *tail;
+};
+
+#endif
